8b9f30b5a121ffb4b03acd452a98fe737b9a5d6e
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2020-10-20 20:41:15 +0200

    fix lib32-mesa repository detection

--- buildmesa.sh
+++ buildmesa.sh
@@ -10,7 +10,7 @@ cd "$PKGDIR/mesa/repos/extra-x86_64" || exit
 git pull
 makepkg -sic --noconfirm
 
-[ ! -d lib32 ] && asp checkout lib32-mesa
+[ ! -d lib32-mesa ] && asp checkout lib32-mesa
 cd "$PKGDIR/lib32-mesa/repos/multilib-x86_64" || exit
 git pull
 makepkg -sic --noconfirm
