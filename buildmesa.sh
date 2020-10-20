0316e63c7a9cb0367654bb3663828106292941fe
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2020-10-20 20:27:30 +0200

    add buildmesa.sh

---
+++ buildmesa.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+PKGDIR="$HOME/devel/pkg"
+cd "$PKGDIR" || exit
+
+asp update mesa lib32-mesa
+
+[ ! -d mesa ] && asp checkout mesa
+cd "$PKGDIR/mesa/repos/extra-x86_64" || exit
+git pull
+makepkg -sic --noconfirm
+
+[ ! -d lib32 ] && asp checkout lib32-mesa
+cd "$PKGDIR/lib32-mesa/repos/multilib-x86_64" || exit
+git pull
+makepkg -sic --noconfirm
+
